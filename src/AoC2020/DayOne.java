eb6c0eda56adbb816c9768ea73d8e4c4f18dc77a
bpomara <ben.omara@gmail.com>
2020-11-30 23:15:01 -0600

    2020 Day One

--- src/AoC2020/DayOne.java
+++ src/AoC2020/DayOne.java
@@ -1,7 +1,47 @@
 package AoC2020;
 
-public class DayOne {
-	public static void main(String[] args){
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class DayOne {
+	public static int[] fileToArray() {
+		try {
+			ArrayList<Integer> inputList = new ArrayList<>();
+			File file = new File("src\\AoC2020\\dayOne.txt");
+			Scanner scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+				String input = scan.nextLine();
+				inputList.add(Integer.parseInt(input));
+			}
+			scan.close();
+			int[] output = new int[inputList.size()];
+			for (int i = 0; i < inputList.size(); i++) {
+				output[i] = inputList.get(i);
+			}
+			return output;
+		}catch (FileNotFoundException error) {
+			System.err.println("Whoops! File not found.");
+			return new int[1];
+		}
+	}
+
+	public static void main(String[] args){
+		int[] expenses = fileToArray();
+		for(int i = 0; i < expenses.length; i++){
+			for (int j = i+1; j < expenses.length; j++){
+				for (int k = j+1; k < expenses.length; k++) {
+					if (expenses[i] + expenses[j] + expenses[k] == 2020) {
+						System.out.println(expenses[i]);
+						System.out.println(expenses[j]);
+						System.out.println(expenses[k]);
+						System.out.println(expenses[i] + expenses[j] + expenses[k]);
+						System.out.println(expenses[i] * expenses[j] * expenses[k]);
+						System.out.println();
+					}
+				}
+			}
+		}
 	}
 }
